ed9433a806938eceb01ea51ade623e088651a0a4
Jeimy Jara <desarrollo1@imporfactoryusa.com>
2025-08-06 16:42:08 -0500

    Config:JS se actualizo para que no se devuelva en UTC

--- src/database/config.js
+++ src/database/config.js
@@ -1,14 +1,27 @@
 const { Sequelize } = require('sequelize');
 
-
 const db = new Sequelize({
-  dialect: process.env.DB_DIALECT,
+  dialect: 'mysql',
   host: process.env.DB_HOST,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
   logging: false,
+  timezone: '-05:00',
+  dialectOptions: {
+    typeCast(field, next) {
+      // Este bloque evita que DATETIME sea convertido a UTC
+      if (field.type === 'DATETIME') {
+        return field.string();
+      }
+      return next();
+    },
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
 });
 
 module.exports = { db };
