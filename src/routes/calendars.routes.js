de3972d71301745026afbc1f459acf036890510f
Michael Ordoñez <desarrollo3imporfactory@gmail.com>
2025-08-19 17:06:53 -0500

    calendar.routes.js: agregando middleware -Sebastian

--- src/routes/calendars.routes.js
+++ src/routes/calendars.routes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
+
 const auth = require('../middlewares/auth.middleware');
 const checkPlanActivo = require('../middlewares/checkPlanActivo.middleware');
 const restrictToPlanes = require('../middlewares/restrictToPlanes.middleware');
+
 const ctrl = require('../controllers/calendars.controller');
 
+//Protecciones globales del router - aplicando a todas las rutas
 router.use(auth.protect);
+router.use(checkPlanActivo);
+router.use(restrictToPlanes([1, 3, 4]));
 
 router.get('/', ctrl.list);
 router.post('/', ctrl.create);
-router.post('/ensure', ctrl.ensure,checkPlanActivo,restrictToPlanes([2,3]));
+router.post('/ensure', ctrl.ensure);
+
 module.exports = router;
