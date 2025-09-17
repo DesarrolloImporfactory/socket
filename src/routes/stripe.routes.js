6583974abbe663c0624d9d379015204e0e2b1eb6
Kevin Becerra <k.becerra@imporfactorylatam.com>
2025-09-17 10:49:12 -0500

    stripe.route.js: se removio checkplanactivo de obtenersuscripcionactiva - Kevin

--- src/routes/stripe.routes.js
+++ src/routes/stripe.routes.js
@@ -14,7 +14,7 @@ router.get('/stripe', stripe_planesController.listarPlanesStripe);
 router.post('/crearSesionPago', stripe_planesController.crearSesionPago);
 router.post('/facturasUsuario', stripe_planesController.obtenerFacturasUsuario);
 router.post('/cancelarSuscripcion', checkPlanActivo, stripe_planesController.cancelarSuscripcion);
-router.post('/obtenerSuscripcionActiva', checkPlanActivo, stripe_planesController.obtenerSuscripcionActiva);
+router.post('/obtenerSuscripcionActiva', stripe_planesController.obtenerSuscripcionActiva);
 router.post('/crearSesionSetupPM', stripe_planesController.crearSesionSetupPM);
 router.post('/portalAddPaymentMethod', stripe_planesController.portalAddPaymentMethod);
 router.post('/portalGestionMetodos', stripe_planesController.portalGestionMetodos);
