8b6ac88c95a58dbae23d9767f2f636a68bca12fb
Jeimy Jara <desarrollo1@imporfactoryusa.com>
2025-10-07 11:08:35 -0500

    validacion 3

--- src/controllers/messenger.controller.js
+++ src/controllers/messenger.controller.js
@@ -24,6 +24,7 @@ exports.receiveWebhook = catchAsync(async (req, res) => {
     console.log('Otra línea de log para verificar el flujo');
     console.log('Otra línea de log para verificar el flujo');
     console.log('Otra línea de log para verificar el flujo');
+    console.log('Otra línea de log para verificar el flujo');
     return res.sendStatus(200);
   }
 
