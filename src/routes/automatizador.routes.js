8c692f1830e9512cb596785433e41acf1f42bd11
Tony Plaza - Desarrollador <desarrollo2imporfactory@gmail.com>
2025-10-31 17:13:24 -0500

    creando endpoints de automatizador - Tony plaza

--- src/routes/automatizador.routes.js
+++ src/routes/automatizador.routes.js
@@ -4,28 +4,6 @@ const express = require('express');
 
 const router = express.Router();
 
-// Cambiar a GET para facilitar las pruebas con query params
-router.get(
-  '/obtenerProductosAutomatizador',
-  automatizadorController.obtenerProductosAutomatizador
-);
-
-router.get(
-  '/obtenerCategoriasAutomatizador',
-  automatizadorController.obtenerCategoriasAutomatizador
-);
-
-router.get(
-  '/obtenerTemplatesAutomatizador',
-  automatizadorController.obtenerTemplatesAutomatizador
-);
-
-router.get(
-  '/obtenerEtiquetasAutomatizador',
-  automatizadorController.obtenerEtiquetasAutomatizador
-);
-
-// También mantener las rutas POST por compatibilidad (usando body)
 router.post(
   '/obtenerProductosAutomatizador',
   automatizadorController.obtenerProductosAutomatizador
