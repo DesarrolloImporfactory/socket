ecb2f0eaf3450eff5b93d0b574557820cf77c055
Michael Ordonez <desarrollo3imporfactory@gmail.com>
2026-04-16 13:12:45 -0500

    syncdropiorderhourly: consulta cada 5 min -sebastian

--- src/cron/syncDropiOrdersHourly.js
+++ src/cron/syncDropiOrdersHourly.js
@@ -1223,7 +1223,7 @@ async function runHourlyDropiSync() {
   }
 }
 
-cron.schedule('* * * * *', () => {
+cron.schedule('*/5 * * * *', () => {
   runHourlyDropiSync().catch((err) =>
     log(`[hourly-dropi] Unhandled: ${err?.message}`).catch(() => {}),
   );
